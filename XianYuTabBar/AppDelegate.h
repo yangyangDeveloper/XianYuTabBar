148c1278758b01cf2564318e5f0244c97c199fe0
yangyangzhang <zyydeveloper@163.com>
2017-04-21 15:56:25 +0800

    Initial Commit

---
+++ XianYuTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XianYuTabBar
+//
+//  Created by Tech-zhangyangyang on 2017/4/21.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XianYuTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XianYuTabBar
+//
+//  Created by Tech-zhangyangyang on 2017/4/21.
+//  Copyright © 2017年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
